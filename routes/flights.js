400144c7f0e5697dc838d516ae43c5ac5232da63
EbbaSchmid <justebba@gmail.com>
2022-09-27 18:37:50 -0500

    routes update

--- routes/flights.js
+++ routes/flights.js
@@ -5,9 +5,21 @@ const router = Router()
 import * as flightsCtrl from '../controllers/flights.js'
 
 // GET /flights/new
+router.get('/', flightsCtrl.index)
+
 router.get('/new', flightsCtrl.new)
 
 router.post('/', flightsCtrl.create)
+
+router.get('/:id', flightsCtrl.show)
+
+router.get('/:id/edit', flightsCtrl.edit)
+
+router.delete('/:id', flightsCtrl.delete)
+
+router.put('/:id', flightsCtrl.update)
+
+router.post('/:id/tickets', flightsCtrl.create)
 
 export {
 	router
