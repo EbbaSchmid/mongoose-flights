61783cba9ce1f32a6d92178644581c72e99e84dc
EbbaSchmid <justebba@gmail.com>
2022-09-27 18:33:48 -0500

    schema updates

--- models/flight.js
+++ models/flight.js
@@ -1,15 +1,35 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
+// optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
+	
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number, min: 0
+  }, 
+})
+
 
 const flightSchema = new Schema({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date,
-  
-}, {
-  timestamps: true
+  airline: {
+    type: String, 
+
+    enum: ['Delta', 'TWA', 'Spirit']
+  },
+  flightNo: {
+    type: Number, required: true
+  },
+  airport: {
+    type: [String]
+  },
+  departs: {
+    type: Date, required: true
+  },
+  tickets: [ticketSchema],
 })
 
 const Flight = mongoose.model('Flight', flightSchema)
