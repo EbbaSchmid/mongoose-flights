458b0661b78f7ea2a70a32baeb002939b7eb2b7d
EbbaSchmid <justebba@gmail.com>
2022-09-27 16:45:21 -0500

    new ejs setup

--- routes/flights.js
+++ routes/flights.js
@@ -2,11 +2,11 @@ import { Router } from 'express'
 
 const router = Router()
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+import * as flightsCtrl from '../controllers/flights.js'
+
+// GET /flights/new
+router.get('/new', flightsCtrl.new)
 
 export {
-  router
+	router
 }
