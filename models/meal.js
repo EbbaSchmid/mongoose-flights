bfb692a062ba6c1f440ce78139e6d6275cb5b2ef
EbbaSchmid <justebba@gmail.com>
2022-09-27 20:13:35 -0500

    model clean up

--- models/meal.js
+++ models/meal.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose'
 
-// optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
-	
+
 const mealSchema = new Schema({
-  meal: {
-    type: String,
-    match: /[A-F][1-9]\d?/
-  },
-  price: {
-    type: Number, min: 0
-  }, 
+  name: String,
+}, {
+  timestamps: true
 })
+
+const Meal = mongoose.model('Meal', mealSchema)
+
+export {
+  Meal
+}
--- routes/meals.js
+++ routes/meals.js
@@ -1,4 +1,17 @@
+import { Router } from 'express'
+import * as mealsCtrl from '../controllers/meals.js'
 
+const router = Router()
+
+/* GET home page. */
+router.get('/new', mealsCtrl.new)
+
+
+router.post('/', mealsCtrl.create)
+
+export { 
+  router
+}
 
 
 
