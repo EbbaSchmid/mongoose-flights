a737938342d851c816d2dd7eaadabad13ce1f0ae
EbbaSchmid <justebba@gmail.com>
2022-09-27 19:25:05 -0500

    ejs update

---
+++ controllers/performers.js
@@ -0,0 +1,31 @@
+import { Performer } from "../models/performer.js"
+
+function newPerformer(req, res) {
+  Performer.find({})
+  .then(performers => {
+    res.render('performers/new', {
+      title: 'Add Performer',
+      performers: performers
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/movies')
+  })
+}
+
+function create(req, res) {
+  Performer.create(req.body)
+  .then(performer => {
+    res.redirect('/performers/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/performers/new')
+  })
+}
+
+export {
+  create,
+  newPerformer as new
+}
